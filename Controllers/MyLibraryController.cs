bf59d3c07a2939b19a5191d00cdaff5f968e15f3
Trent Lyons <tlyons1@cougars.ccis.edu>
2020-09-18 18:35:23 -0500

    Add project files.

---
+++ Controllers/MyLibraryController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Encodings.Web;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Dropbox03.Controllers
+{
+    public class MyLibraryController : Controller
+    { 
+        // GET: /<controller>/
+        static List<string> allBooks = new List<string> { "Java Basic", "Agile with C#", "Advanced Agile" };
+        
+        public IActionResult AllBooks()
+        {
+        ViewData["My Books"] = allBooks;
+        return View();
+        }
+        public IActionResult AddBook()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult AddBook(string bookTitle)
+        {
+            allBooks.Add(bookTitle);
+            return RedirectToAction("Book", "MyLibrary");
+        }
+    }
+}
